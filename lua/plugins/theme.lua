a415c527c8a496449236709d840e9e580782eec8
drew york <119648808+dre-yo@users.noreply.github.com>
2024-09-26 16:37:25 -0700

    fixed color

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,18 +1,19 @@
 -- plugins/theme.lua
-local function setup_nightfox()
-    -- Apply the chosen color scheme directly
-    vim.cmd('colorscheme carbonfox')  -- You can change 'nightfox' to 'dayfox', etc.
-    
+local function setup_nightfox()    
     -- Setup options for nightfox if supported
     require('nightfox').setup({
         options = {
-           transparent = true, -- Set to true if you want a transparent background
+           transparent = false, -- Set to true if you want a transparent background
+	   terminal_colors = true,
             styles = {
                 comments = "italic",  -- Example of custom styling
                 keywords = "bold",
+		types = "italic,bold",
             },
         },
     })
+    -- Apply the chosen color scheme directly
+    vim.cmd('colorscheme carbonfox')  -- You can change 'nightfox' to 'dayfox', etc.
 end
 
 return {
