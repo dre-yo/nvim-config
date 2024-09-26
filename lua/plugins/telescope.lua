a4a6e4391264a46f20c065415a19bf4b761fcb63
drew york <119648808+dre-yo@users.noreply.github.com>
2024-09-26 15:04:25 -0700

    first commit

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,22 @@
+-- plugins/telescope.lua
+local function setup_telescope()
+    local telescope = require('telescope')
+
+    -- Setup telescope with any configuration options you may need
+    telescope.setup {}
+
+    -- Require the builtin functions for key mappings
+    local builtin = require('telescope.builtin')
+
+    -- Define your key mappings
+    vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+    vim.keymap.set('n', '<leader>fg', builtin.git_files, { desc = 'Telescope find fit files' })
+end
+
+return {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.8',  -- or use branch = '0.1.x'
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = setup_telescope,  -- Call the setup function when the plugin is loaded
+}
+
