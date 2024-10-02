c1038f991384eab93032a2b5f9d37912caaabab4
drew york <119648808+dre-yo@users.noreply.github.com>
2024-10-02 01:04:54 -0700

    changed harpoon so tmux can work

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -10,10 +10,10 @@ local function setup_harpoon()
 	vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
 	vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-	vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-	vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end)
-	vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end)
-	vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end)
+  vim.keymap.set("n", "<S-Left>", function() harpoon.list():select(1) end)
+  vim.keymap.set("n", "<S-Down>", function() harpoon.list():select(2) end)
+  vim.keymap.set("n", "<S-Up>", function() harpoon.list():select(3) end)
+  vim.keymap.set("n", "<S-Right>", function() harpoon.list():select(4) end)
 
 	-- Toggle previous & next buffers stored within Harpoon list
 	vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
