383abdf2009deb13cdded6638b57208cabb1cc13
drew york <119648808+dre-yo@users.noreply.github.com>
2024-10-03 11:18:21 -0700

    added new git gutter

--- lua/plugins/gitgutter.lua
+++ lua/plugins/gitgutter.lua
@@ -1,3 +1,35 @@
+local function setup_gitsigns()
+  require('gitsigns').setup({
+    signs = {
+      add          = { text = '┃' },
+      change       = { text = '┃' },
+      delete       = { text = '_' },
+      topdelete    = { text = '‾' },
+      changedelete = { text = '~' },
+      untracked    = { text = '┆' },
+    },
+    signs_staged = {
+      add          = { text = '┃' },
+      change       = { text = '┃' },
+      delete       = { text = '_' },
+      topdelete    = { text = '‾' },
+      changedelete = { text = '~' },
+      untracked    = { text = '┆' },
+    },
+    signcolumn = true,
+    current_line_blame = true,
+    current_line_blame_opts = {
+      virt_text = true,
+      virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+      delay = 1000,
+      ignore_whitespace = false,
+      virt_text_priority = 100,
+      use_focus = true,
+    },
+    current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
+  })
+end
 return {
-  "airblade/vim-gitgutter",
+  'lewis6991/gitsigns.nvim',
+  config = setup_gitsigns
 }
