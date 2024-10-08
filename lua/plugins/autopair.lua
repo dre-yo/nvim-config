e766302930681e498fc7d9d4ed076a9922ff0593
drew york <119648808+dre-yo@users.noreply.github.com>
2024-10-08 15:46:29 -0700

    fixed autopair not loading issue

--- lua/plugins/autopair.lua
+++ lua/plugins/autopair.lua
@@ -1,6 +1,7 @@
 return {
   'windwp/nvim-autopairs',
   event = "InsertEnter",
+  lazy = false,
   config = true
   -- use opts = {} for passing setup options
   -- this is equivalent to setup({}) function
